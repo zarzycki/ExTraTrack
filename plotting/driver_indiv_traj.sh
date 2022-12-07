1e3ab27b4bcf2ec86d39588ca5749a12ab528735
Colin Zarzycki <zarzycki@ucar.edu>
2022-12-06 21:29:16 -0500

    clean up some of the plotting code

--- plotting/driver_indiv_traj.sh
+++ plotting/driver_indiv_traj.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-TYPE="ape"
+TYPE="nx4"      # type from namelist (i.e., what dataset to plot
+DAT="avg"       # plot avg (smoothed) or orig -- Z2017 showed avg
+DOMAIN="natl"   # glob, nhemi, natl (or add a new domain to ncl file below)
+
 FILE=../et-tracker/climatology_files/etdetails_${TYPE}.txt
 
 while read line; do
@@ -8,10 +11,24 @@ while read line; do
   stringarray=($line)
   itcstr=${stringarray[0]}
   yyyy=${stringarray[5]}
+  
   mm1=`printf "%02d\n" $((10#${stringarray[6]}-10#1))`
+
   mm2=`printf "%02d\n" $((10#${stringarray[10]}+10#1))`
+    
   date_start="${stringarray[5]} ${stringarray[6]} ${stringarray[7]} ${stringarray[8]}"
+  
   date_end="${stringarray[9]} ${stringarray[10]} ${stringarray[11]} ${stringarray[12]}"
-  echo ncl et_individual_nx_traj.ncl 'type="'${TYPE}'"' 'itcstr="'${itcstr}'"' 'yyyy="'${yyyy}'"' 'mm1="'${mm1}'"' 'mm2="'${mm2}'"' date_start=\""$date_start\"" date_end=\""$date_end\""
-  ncl et_individual_nx_traj.ncl 'type="'${TYPE}'"' 'itcstr="'${itcstr}'"' 'yyyy="'${yyyy}'"' 'mm1="'${mm1}'"' 'mm2="'${mm2}'"' date_start=\""$date_start\"" date_end=\""$date_end\""
+    
+  (set -x; ncl et_individual_nx_traj.ncl 'type="'${TYPE}'"' \
+    'dat="'${DAT}'"' \
+    'plot_domain="'${DOMAIN}'"' \
+    'itcstr="'${itcstr}'"' \
+    'yyyy="'${yyyy}'"' \
+    'mm1="'${mm1}'"' \
+    'mm2="'${mm2}'"' \
+    date_start=\""$date_start\"" \
+    date_end=\""$date_end\"" \
+  )
+  
 done < $FILE
