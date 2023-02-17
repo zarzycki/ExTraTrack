00774f0b7b7af091c40e82cd1322b493483fc46c
Colin Zarzycki <zarzycki@ucar.edu>
2023-02-17 08:37:55 -0700

    refactor gen-files a bit

--- et-tracker/filelists/gen-files.sh
+++ et-tracker/filelists/gen-files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DIR=/storage/home/cmz5202/scratch/z.day1.0/run/ET/
+OUTNAME=/glade/u/home/zarzycki/sw/ExTraTrack/et-tracker/filelists/files.dtime900.002.txt
+DIR=/glade/u/home/zarzycki/scratch/hyperion/ET2_CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900.002
 
-find ${DIR} -name "*.nc" | sort -n > filelist.txt
+find ${DIR} -name "*.nc" | sort -n > ${OUTNAME}
