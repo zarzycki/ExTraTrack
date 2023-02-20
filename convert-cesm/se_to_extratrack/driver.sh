ea41c8b087e819e87740e0d6b0820873e09bdeb1
Colin Zarzycki <zarzycki@ucar.edu>
2023-02-20 05:03:45 -0700

    more updates to CESM -> ET converter

--- convert-cesm/se_to_extratrack/driver.sh
+++ convert-cesm/se_to_extratrack/driver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 ################################################################
-#PBS -N test_gnu
-#PBS -A P93300042 
+#PBS -N ET_convert_cesm
+#PBS -A P93300042
 #PBS -l walltime=10:49:00
 #PBS -q premium
 #PBS -k oe
-#PBS -m a 
+#PBS -m a
 #PBS -M zarzycki@ucar.edu
 #PBS -l select=1:ncpus=36:mem=109GB
 ################################################################
@@ -26,17 +26,19 @@ starttime=$(date -u +"%s")
 
 module load parallel
 
+NLFILE=nl.hyperion
 NUMCORES=8
 TIMESTAMP=`date +%s%N`
 COMMANDFILE=commands.${TIMESTAMP}.txt
+CASENAME=`grep -hnr "casename" ${NLFILE} | cut -d'"' -f 2`
 
-FILES=`find /glade/u/home/zarzycki/scratch/hyperion/CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900/atm/ -name "*cam.h2.*.nc" | sort -n`
+FILES=`find /glade/u/home/zarzycki/scratch/hyperion/${CASENAME}/ -name "*cam.h2.*.nc" | sort -n`
 
 cd /glade/u/home/zarzycki/sw/ExTraTrack/convert-cesm/se_to_extratrack
 
 for f in ${FILES}
 do
-  NCLCOMMAND="ncl create-files.ncl 'f2name=\"'${f}'\"' 'nlfile=\"nl.hyperion\"'     "
+  NCLCOMMAND="ncl create-files.ncl 'f2name=\"'${f}'\"' 'nlfile=\"'${NLFILE}'\"'     "
   echo ${NCLCOMMAND} >> ${COMMANDFILE}
 done
 
