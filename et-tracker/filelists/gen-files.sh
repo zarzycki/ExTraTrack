7958da8559952bf562dc020a81cb5fba6e3175cb
Colin Zarzycki <zarzycki@ucar.edu>
2023-10-06 07:51:16 -0600

    generalize gen-files tool

--- et-tracker/filelists/gen-files.sh
+++ et-tracker/filelists/gen-files.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-OUTNAME=/glade/u/home/zarzycki/sw/ExTraTrack/et-tracker/filelists/files.dtime900.002.txt
-DIR=/glade/u/home/zarzycki/scratch/hyperion/ET2_CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900.002
+# ./gen-files.sh -o files.ERA5.txt -d ~/scratch/h1files/ERA5v3/ -p "*.h1.2015*.nc"
 
-find ${DIR} -name "*.nc" | sort -n > ${OUTNAME}
+# Defaults
+OUTNAME=files.ERA5.txt
+DIR=~/scratch/h1files/ERA5v3/
+PATTERN="*.nc"
+
+# Parse anything the user passed in
+while getopts "o:d:p:" opt; do
+    case $opt in
+        o) OUTNAME="$OPTARG";;
+        d) DIR="$OPTARG";;
+        p) PATTERN="$OPTARG";;
+        \?) echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :) echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+echo "DIR: $DIR"
+echo "PATTERN $PATTERN"
+echo "OUTNAME $OUTNAME"
+
+find "${DIR}" -name "${PATTERN}" | sort -n > "${OUTNAME}"
